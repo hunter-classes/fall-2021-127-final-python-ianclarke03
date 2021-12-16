d9be4e3cc5639ec3db2652c42aff29d9d609b8c2
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2021-12-16 17:04:06 +0000

    1 and 3 done

--- answers.py
+++ answers.py
@@ -2,14 +2,15 @@ print("------------------- Question 1 ----------------")
 print("this function takes a list and returns True if the items in the list are continually increasing, and False if they are not")
 
 def isIncreasing(list):
-  for item in list:
-    if list[item] <= list[item + 1]:
+  for item in range(len(list)):
+    if list[item] >= list[item + 1]:
       return "False"
-    else:
+    elif list[item] < list[item + 1]:
       return "True"
 
-print(isIncreasing([1,2,3,4,5]))
-
+print("The list [1,2,3,4,5] is continually increasing." , isIncreasing([1,2,3,4,5]))
+print("The list [6,2,11,4,5] is continually increasing." , isIncreasing([6,2,11,4,5]))
+print("The list [2,4,6,8,10] is continually increasing." , isIncreasing([2,4,6,8,10]))
 
 
 
@@ -29,39 +30,22 @@ print(isIncreasing([1,2,3,4,5]))
 #If you are totally stuck on this, DM me on Zulip and I will provide a
 #hint but you won't be able to get full credit if you take the hint.
 
-#print("------------------- Question 2 ----------------")
+print("------------------- Question 2 ----------------")
 
-#def NumConvert(list):
-#  str = ""
-#  for num in range(len(list)):
-#    str += num
-#  return (str)
+def NumConvert(list):
+  str = ""
+  for num in range(len(list)):
+    str += num
+  return (str)
   
-#print(NumConvert([3,5,1]))
+print(NumConvert([3,5,1]))
 
 
 
 
-print("------------------- Question 1 ----------------")
-print("this function takes a list and returns True if the items in the list are continually increasing, and False if they are not")
+print("------------------- Question 3 ----------------")
+print("this function takes a string representing a binary number and returns an integer of that number converted into a decimal.")
 
-# 3. BinConvert (20pts)
-
-#Write a function named *BinConvert* that takes a string representing a
-#binary number and returns an integer with that number converted to
-#decimal.
-
-
-#Examples:
-
-#| Call               | Returns |
-#|--------------------+---------|
-#| BinConvert("1")    |       1 |
-#| BinConvert("10")   |       2 |
-#| BinConvert("11")   |       3 |
-#| BinConvert("100")  |       4 |
-#| BinConvert("101")  |       5 |
-#| BinConvert("1011") |      11 |
 
 def BinConvert(bin):
   place = 0
@@ -74,5 +58,7 @@ def BinConvert(bin):
       place += 1
   return result
 
-print (BinConvert('1011'))
-
+print ("1011 is converted into" , BinConvert('1011'))
+print ("101 is converted into" , BinConvert('101'))
+print ("10 is converted into" , BinConvert('10'))
+print ("1 is converted into" , BinConvert('1'))
